a77c61db6c5bb4633c9840734639eca119e0fc7f
T8ninho <tonisantos.contato@gmail.com>
2023-12-23 00:07:50 -0300

    modified:   .gitignore 	modified:   src/Tarefas/Tarefas.js 	renamed:    src/Tarefas/firebase.js -> src/Tarefas/firebase/firebase.js

--- src/Tarefas/Tarefas.js
+++ src/Tarefas/Tarefas.js
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
-import {db} from './firebase';
+import {db} from './firebase/firebase';
 import { Input, Button, Switch } from 'antd';
 
 const useFirestoreCollection = (collection) => {
